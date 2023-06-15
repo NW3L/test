727bbf92c95fcba38064f00898a1225895bc6c25
NW3L <120674332+NW3L@users.noreply.github.com>
2023-06-15 22:38:29 +0300

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -1,9 +1,14 @@
 import os
-
+import time
+# запускаем test1.py
 os.system("python3.10 test/test1.py")
+time.sleep(60)
 
+# запускаем test2.py, если test1.py завершился успешно
 if os.WEXITSTATUS(os.system("echo $?")) == 0:
     os.system("python3.10 test/test2.py")
+time.sleep(120)
 
+# запускаем test3.py, если test2.py завершился успешно
 if os.WEXITSTATUS(os.system("echo $?")) == 0:
     os.system("python3.10 test/test3.py")
