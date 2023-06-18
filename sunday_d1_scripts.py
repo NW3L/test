613561ad45235a0f6aa688003b17bb2d56ce81f7
NW3L <120674332+NW3L@users.noreply.github.com>
2023-06-18 15:38:59 +0300

    Delete sunday_d1_scripts.py

--- sunday_d1_scripts.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-# d1 all download
-os.system("python3.10 test/0_month_d1_download.py")
-time.sleep(120)
-
-# d1 base update
-if os.WEXITSTATUS(os.system("echo $?")) == 0:
-    os.system("python3.10 test/0_month_d1_update.py")
